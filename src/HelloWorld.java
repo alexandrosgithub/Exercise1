98126bd7452e3a1ecc8f0d69098ba5974d880894
alexandrosgithub <alxtsalidis@gmail.com>
2017-11-06 00:03:01 +0100

    secondcommit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,17 @@
-
+import java.util.Scanner;
 public class HelloWorld {
 
+	private static HelloUser user;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.println("Hello ASE2017 - How are you now? :)");
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter your name: ");
+		user = new HelloUser(keyboard.nextLine());
+		user.greetUser();
 	}
 
+	
 }
